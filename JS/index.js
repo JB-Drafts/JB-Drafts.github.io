d99bf0b6004b8d9f0448e4da42d097131a18fe6a
JB-Drafts <jb-drafts@outlook.com>
2025-01-21 15:16:54 -0400

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,1 @@
-alert("disculpe los incomvenientes hacerca de como esta pagina se muestra en su pantalla,  esta pagina fue pensada para computadores, proximamente la disfrutara en su telefono")
-
-alert("por favor gire su pantalla para interactuar mejor,Gracias")
+alert("disculpe los incomvenientes hacerca de como esta pagina se muestra en su pantalla, proximamente la disfrutara en su telefono, por favor gire su pantalla para interactuar mejor, Gracias)
